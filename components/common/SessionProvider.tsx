8bece29793d62805ec9d87e0fa9b5b6a34edccee
geraldofori <oforige@gmail.com>
2025-11-23 16:54:21 +0000

    chore: added session provider context and updated the login flow

---
+++ components/common/SessionProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import type { Session } from "next-auth";
+import { SessionProvider as Provider } from "next-auth/react";
+import type { UserSession } from "@/models";
+
+export interface Props {
+  readonly children: React.ReactNode;
+  readonly session: UserSession;
+}
+
+export default function SessionProvider({ children, session }: Props) {
+  return (
+    <Provider session={session}>{children}</Provider>
+  );
+}
