71048acd3c993b3afe87a62e6396e1a6a29e5e34
geraldofori <oforige@gmail.com>
2025-12-02 17:30:55 +0000

    chore: implemented ui updates on the mobile navigation and web navigations

--- constants/index.ts
+++ constants/index.ts
@@ -2,12 +2,11 @@ import { EventData, Prayer, PrayerRequest } from "@/models";
 
 export enum PAGE_URL {
   PRAYER_REQUEST = "",
-  ADMIN_LOGIN = "/prayer-request/admin/"
+  ADMIN_LOGIN = "/prayer-request/admin/",
 }
 export enum API_BASE_URL {
   PRAYER = "https://newlife-prayer-wall-backend-1gq0w2ror.vercel.app/",
 }
-
 
 export const events: EventData[] = [
   {
